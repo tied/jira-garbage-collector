a87de9342be6fc9c675e490e161b5b68c073c3c5
wyd3x <shuhss@gmail.com>
2020-03-19 19:39:41 +0200

    Licensing + logos

--- src/main/java/com/assertteam/rest/utils/Auth.java
+++ src/main/java/com/assertteam/rest/utils/Auth.java
@@ -1,5 +1,7 @@
 package com.assertteam.rest.utils;
 
+import com.atlassian.upm.api.license.entity.PluginLicense;
+import com.atlassian.upm.api.util.Option;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
@@ -8,21 +10,25 @@ import com.atlassian.jira.security.GlobalPermissionManager;
 import com.atlassian.jira.security.JiraAuthenticationContext;
 import org.springframework.beans.factory.annotation.Autowired;
 import com.atlassian.plugin.spring.scanner.annotation.imports.ComponentImport;
+import com.atlassian.upm.api.license.PluginLicenseManager;
 
 @Component
 public class Auth {
     private JiraAuthenticationContext jiraAuthenticationContext;
     private GlobalPermissionManager globalPermissionManager;
     private Logger logger = LoggerFactory.getLogger(Auth.class);
+    private PluginLicenseManager pluginLicenseManager;
 
     @Autowired
-    public Auth(@ComponentImport JiraAuthenticationContext jiraAuthenticationContext, @ComponentImport GlobalPermissionManager globalPermissionManager) {
+    public Auth(@ComponentImport JiraAuthenticationContext jiraAuthenticationContext, @ComponentImport GlobalPermissionManager globalPermissionManager, @ComponentImport PluginLicenseManager pluginLicenseManager) {
         this.globalPermissionManager = globalPermissionManager;
         this.jiraAuthenticationContext = jiraAuthenticationContext;
+        this.pluginLicenseManager = pluginLicenseManager;
     }
 
     public boolean canAccess() {
-        if (this.jiraAuthenticationContext.isLoggedInUser()) {
+        Option<PluginLicense> license = this.pluginLicenseManager.getLicense();
+        if (this.jiraAuthenticationContext.isLoggedInUser() && license.isDefined() && !license.get().getError().isDefined()) {
             return this.globalPermissionManager.hasPermission(GlobalPermissionKey.ADMINISTER, this.jiraAuthenticationContext.getLoggedInUser());
         }
         return false;
